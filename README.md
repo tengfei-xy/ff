8c2bbfda13948c3b86a6d321558d8a66b39eecd1
浔阳陌客 <virtualtengfei@163.com>
2023-11-27 14:28:50 +0800

    v0.2.0 添加-a选项

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 用rust写的文件查找工具
 
+## 安装
+```bash
+cargo install tengfei-ff
+```
+
 ## 使用方式
 
 查找文件
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::OnceLock;
 use std::{
     env,
     ffi::OsStr,
@@ -6,22 +7,32 @@ use std::{
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "ff", version="0.1" ,about = "查找文件(rust tools)")]
+#[structopt(name = env!("CARGO_PKG_NAME"), version=env!("CARGO_PKG_VERSION") ,about = env!("CARGO_PKG_DESCRIPTION"))]
 struct Opt {
-
-    #[structopt(short = "f", long = "file",  help = "指定文件名")]
+    #[structopt(short = "f", long = "file", help = "指定文件名")]
     filename: String,
+
+    #[structopt(short = "a", long = "output-full", help = "输出完整地址")]
+    of: bool,
+}
+static OPT: OnceLock<Opt> = OnceLock::new();
+
+fn init() {
+    std::thread::spawn(|| {
+         OPT.get_or_init(|| {
+            Opt::from_args()
+        });
+    }).join().unwrap();
 }
 fn main() {
     // 解析命令参数
-    let opt = Opt::from_args();
     
+    init();
+    
+    let bind = env::current_dir().expect("获取工作路径失败");
+    let path_buf = bind.as_os_str();
 
-
-    let binding = env::current_dir().expect("获取工作路径失败");
-    let path_buf: &OsStr = binding.as_os_str();
-
-    find(path_buf, &opt.filename);
+    find(path_buf, OPT.get().unwrap().filename.as_str());
 }
 fn find(path: &OsStr, target: &str) {
     find_current_path(path, target);
@@ -38,7 +49,9 @@ fn find_depth_path(path: &OsStr, target: &str) {
     }
 }
 fn find_current_path(path: &OsStr, target: &str) {
-    let entries = fs::read_dir(path).map_err(|err| format!("{:?}, {}", path,err)).expect("读取目录失败");
+    let entries = fs::read_dir(path)
+        .map_err(|err| format!("{:?}, {}", path, err))
+        .expect("读取目录失败");
     for entry in entries {
         let entry = entry.expect("错误");
         let file_type = entry.file_type().expect("获取文件类型错误");
@@ -49,12 +62,18 @@ fn find_current_path(path: &OsStr, target: &str) {
 
         let str_file_name = file_name.to_str().expect("转换string失败").to_lowercase();
         if str_file_name.contains(target.to_lowercase().as_str()) {
-            println!(
-                "{}\t\t{}/{}",
-                str_file_name,
-                entry.path().to_str().expect("获取错误"),
-                str_file_name
-            );
+            output(&entry, &str_file_name);
         }
+    }
+}
+fn output(entry: &fs::DirEntry, str_file_name: &str) {
+    if OPT.get().unwrap().of {
+        println!("{}", entry.path().to_str().expect("获取错误"));
+    } else {
+        println!(
+            "{}\t\t{}",
+            str_file_name,
+            entry.path().to_str().expect("获取错误")
+        );
     }
 }
